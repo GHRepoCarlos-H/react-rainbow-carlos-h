48d5070f5676b9e4026796b1ac6ce72710c0a941
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-12-12 09:52:24 -0800

    Completed project

---
+++ src/ColorForm.js
@@ -0,0 +1,24 @@
+//ColorForm.js
+
+import React, { useState } from 'react';
+
+function ColorForm(props) {
+  let [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addColor(input); // Use the correct case for addColor
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Submit!</button>
+      </form>
+    </div>
+  );
+}
+
+export default ColorForm;
+
